874583d436d8f820808e44d0dbe3d678c3568cc0
kkAtGithub <zky.nais@gmail.com>
2020-12-14 21:43:29 +0800

    update v0.9

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,18 +3,24 @@ package com.company;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author wind
  */
 public class Main {
-    private static final String INPUT_DIRECTORY = "\\input\\";
-    private static final String OUTPUT_DIRECTORY = "\\output\\";
-    private static final String OUTPUT_FILE_FORMAT = "webp";
+    private static final String CONFIG_FILE_NAME = "config.properties";
+    private static String WORK_DIRECTORY;
+    private static String FILE_PATH_DELIMITER = "/";
+    private static String INPUT_DIRECTORY = "\\input\\";
+    private static String OUTPUT_DIRECTORY = "\\output\\";
+    private static String OUTPUT_FILE_FORMAT = "webp";
+    private static final String DEFAULT_CONFIG = "INPUT_DIRECTORY=input\n" +
+                                                    "OUTPUT_DIRECTORY=output\n" +
+                                                    "OUTPUT_FILE_FORMAT=webp\n";
 
 
 
@@ -59,7 +65,7 @@ public class Main {
                     BufferedImage image = ImageIO.read(entry);
                     if (image != null) {
                         String entryName = entry.getName();
-                        String outputImage = outputPath + '\\'
+                        String outputImage = outputPath + FILE_PATH_DELIMITER
                                 + entryName.substring(0, entryName.lastIndexOf('.')+1) + OUTPUT_FILE_FORMAT;
                         if (!isExists(outputImage, false)) {
                             newImagesList.add(new ImageWithPath(outputImage, image));
@@ -72,9 +78,8 @@ public class Main {
         return newImagesList;
     }
 
-    public static boolean convertTask(ImageWithPath imageToConvert) {
+    private static boolean convertTask(ImageWithPath imageToConvert) {
         File newConvertTask = new File(imageToConvert.getImagePath());
-        // Obtain a WebP ImageWriter instance
         try {
             ImageIO.write(imageToConvert.getImage(), OUTPUT_FILE_FORMAT, newConvertTask);
         } catch (IOException e) {
@@ -84,16 +89,44 @@ public class Main {
         return true;
     }
 
+    private static void initialize() throws IOException {
+        WORK_DIRECTORY = System.getProperty("user.dir");
+        if (System.getProperty("os.name").contains("Windows")) {
+            FILE_PATH_DELIMITER = "\\";
+        }
+        File propertiesFile = new File(CONFIG_FILE_NAME);
+        if (!propertiesFile.exists()) {
+            FileOutputStream genConfigFile = new FileOutputStream(propertiesFile);
+            byte[] defaultConfToBytes = DEFAULT_CONFIG.getBytes();
+            genConfigFile.write(defaultConfToBytes);
+            genConfigFile.close();
+        }
+        Properties properties = new Properties();
+        BufferedReader readConf = new BufferedReader(new FileReader(propertiesFile));
+        properties.load(readConf);
+
+        INPUT_DIRECTORY = properties.getProperty("INPUT_DIRECTORY");
+        OUTPUT_DIRECTORY = properties.getProperty("OUTPUT_DIRECTORY");
+        OUTPUT_FILE_FORMAT = properties.getProperty("OUTPUT_FILE_FORMAT");
+    }
+
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("用户的当前工作目录:\n"+System.getProperty("user.dir"));
-        List<ImageWithPath> newImagesList = getNewImage(System.getProperty("user.dir"), true);
+        try {
+            initialize();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<ImageWithPath> newImagesList = getNewImage(WORK_DIRECTORY, true);
         System.out.println(newImagesList);
         for (ImageWithPath entry:newImagesList){
             System.out.println(entry.getImagePath());
             System.out.println(entry.getImage());
             convertTask(entry);
         }
+        System.out.println(INPUT_DIRECTORY);
+        System.out.println(OUTPUT_DIRECTORY);
+        System.out.println(OUTPUT_FILE_FORMAT);
     }
 }
